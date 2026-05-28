26c70c284bc17249a603b55a6aee964a6ddca203
Steeven HOUNKPE-SAGBO <e.hounkpesagbo@ecoles-epsi.net>
2026-05-28 18:21:09 +0200

    Ajout du fichier en example

--- src/router/index.js
+++ src/router/index.js
@@ -41,14 +41,14 @@ const router = createRouter({
   ],
 })
 
-router.beforeEach((to, from, next) => {
+router.beforeEach((to) => {
   const authStore = useAuthStore()
+
   if (to.meta.requiresAdmin) {
     if (!authStore.user) {
-      return next('/login')
+      return '/login'
     }
   }
-  next()
 })
 
 export default router
